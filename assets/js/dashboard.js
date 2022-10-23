3d4b6f7f067f62ff207a1268dc18c077b07a9a8b
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-10-23 11:05:25 +0200

    Add mock data
    I've included data regarding the tools and
    user contributions, that I can then render
    as charts on the page.

---
+++ assets/js/dashboard.js
@@ -0,0 +1,121 @@
+// The following data was produced in my Juypter notebook.
+
+const missingFieldStats = {
+  totalTools: 2702,
+  toolsMissingInfo: 2701,
+  howMuchIsMissing: {
+    // number of fields missing : number of tools for which this is true
+    0: 1,
+    1: 1,
+    3: 7,
+    4: 6,
+    5: 10,
+    6: 23,
+    7: 32,
+    8: 68,
+    9: 91,
+    10: 108,
+    11: 759,
+    12: 450,
+    13: 1146,
+  },
+  whatIsMissing: {
+    tool_type: 1671,
+    repository: 1893,
+    user_docs_url: 1958,
+    available_ui_languages: 2363,
+    bugtracker_url: 2419,
+    developer_docs_url: 2587,
+    for_wikis: 2636,
+    api_url: 2647,
+    feedback_url: 2653,
+    icon: 2669,
+    wikidata_qid: 2679,
+    translate_url: 2681,
+    privacy_policy_url: 2692,
+  },
+};
+
+// I'm thinking pie charts for those.
+
+// Mock data sets now
+
+const globalContributions = {
+  thisMonth: 73,
+  allTime: 226,
+};
+
+const mockUser = {
+  user: "NicoleLBee",
+  contributions: {
+    thisMonth: 4,
+    total: 7,
+  },
+  latestActivity: [
+    {
+      toolName: "pywikibot",
+      toolTitle: "Pywikibot",
+      fieldEdited: "available_ui_languages",
+      dateModified: 1666514747862,
+    },
+    {
+      toolName: "mm_wikidata_todo",
+      toolTitle: "Wikidata Todo",
+      fieldEdited: "for_wikis",
+      dateModified: 1652227200000,
+    },
+    {
+      toolName: "mm_find_duplicate_items",
+      toolTitle: "Find duplicate items",
+      fieldEdited: "repository",
+      dateModified: 1651449600000,
+    },
+    {
+      toolName: "pywikibot",
+      toolTitle: "Pywikibot",
+      fieldEdited: "icon",
+      dateModified: 1651447200000,
+    },
+  ],
+};
+// base URL for tool https://toolhub.wikimedia.org/tools/
+
+// base URL for users: https://en.wikipedia.org/wiki/User:
+
+const globalActivity = [
+  {
+    user: "DannyBoyyy77",
+    link: "https://en.wikipedia.org/wiki/User:DannyBoyyy77",
+    toolName: "xtools-ec",
+    toolTitle: "XTools Edit Counter",
+    fieldEdited: "icon",
+  },
+  {
+    user: "Ellenello",
+    link: "https://en.wikipedia.org/wiki/User:Ellenello",
+    toolName: "metawiki-jon-harald-søby-diffedit",
+    toolTitle: "diffedit",
+    fieldEdited: "repository",
+  },
+  {
+    user: "DannyBoyyy77",
+    link: "https://en.wikipedia.org/wiki/User:DannyBoyyy77",
+    toolName: "xtools-ec",
+    toolTitle: "XTools Edit Counter",
+    fieldEdited: "privacy_policy_url",
+  },
+  {
+    user: "Javier Alejandro Herrera Carvajal",
+    link: "https://en.wikipedia.org/wiki/User:Javier%20Alejandro%20Herrera%20Carvajal",
+    toolName: "toolforge-croptool",
+    toolTitle: "CropTool",
+    fieldEdited: "available_ui_languages",
+  },
+  {
+    user: "Tabby578",
+    link: "https://en.wikipedia.org/wiki/User:Tabby578",
+    toolName: "pywikibot",
+    toolTitle: "Pywikibot",
+    fieldEdited: "tool_type",
+  },
+];
