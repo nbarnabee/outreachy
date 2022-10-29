6ed36f877083eac33741af34a06762c96ac9eb24
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-10-29 12:42:47 +0200

    Refactor populateTable function
    When attempting to test the populateTable function, it
    became clear that it was attempting to do far too much.
    Broke it down into a series of smaller, more testable
    functions.

--- tests/leaderboard_copy.js
+++ tests/leaderboard_copy.js
@@ -31,26 +31,53 @@ const leaderboard = [
   },
 ];
 
+/* For each object within an array of objects, this function returns a <tr> element that contains two <td> elements:  one containing the user name and a link to the user's profile, and the other containing the user's score */
+
 function populateTable(tableID) {
   sortedLeaderboard = leaderboard.sort((a, b) => b[tableID] - a[tableID]);
   const tableBody = document.getElementById(tableID);
   for (let entry of sortedLeaderboard) {
     let row = document.createElement("tr");
-    let userEntry = document.createElement("td");
-    let score = document.createElement("td");
-    let user = document.createElement("span");
-    let userLink = document.createElement("a");
-    userLink.setAttribute("href", entry.link);
-    userLink.setAttribute("target", "_blank");
-    user.innerText = entry.user;
-    score.innerText = entry[tableID];
     tableBody.appendChild(row);
-    row.appendChild(userEntry);
-    userEntry.appendChild(userLink);
-    userLink.appendChild(user);
-    row.appendChild(score);
+    row.appendChild(createUserEntry(entry));
+    row.appendChild(createScoreEntry(entry, tableID));
   }
+}
+
+/* This function accepts a user object and returns a <td> 
+element that contains the user name and a profile link */
+
+function createUserEntry(entry) {
+  let userEntry = document.createElement("td");
+  let userLink = createUserLink(entry);
+  userEntry.appendChild(userLink);
+  let user = document.createElement("span");
+  user.innerText = entry.user;
+  userLink.appendChild(user);
+  return userEntry;
+}
+
+/* This function accepts a user object and creates a link to a user's profile that is passed back to createUserEntry() */
+
+function createUserLink(entry) {
+  let userLink = document.createElement("a");
+  userLink.setAttribute("href", entry.link);
+  userLink.setAttribute("target", "_blank");
+  return userLink;
+}
+
+/* This function accepts a user object and a string 
+referencing the table that's being populated, and 
+creates a <td> element containing the user's 
+score that is passed back to createUserEntry() */
+
+function createScoreEntry(entry, tableID) {
+  let score = document.createElement("td");
+  score.innerText = entry[tableID];
+  return score;
 }
 
 populateTable("latest");
 populateTable("allTime");
+
+module.exports = { populateTable, createUserEntry };
